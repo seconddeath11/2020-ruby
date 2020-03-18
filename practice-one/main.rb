74b42af8c4ee3cc226dca779d21c8be3cb559ea3
Diana Novikova <novikova_diana2000@mail.ru>
2020-03-18 12:55:13 +0300

    Final

--- practice-one/main.rb
+++ practice-one/main.rb
@@ -1,3 +1,12 @@
 require_relative 'my_complex.rb'
-complex1 = MyComplex.new
-pp complex1
+
+def main
+    complex1 = MyComplex.new(1, 1)
+    complex2 = MyComplex.new(2, 4)
+    puts complex1, complex2
+    puts "Сумма: #{complex1.add(complex2)}"
+end 
+
+if __FILE__ == $0
+    main
+end
--- practice-one/my_complex.rb
+++ practice-one/my_complex.rb
@@ -1,3 +1,30 @@
 class MyComplex
+    def initialize(real, imaginary)
+        @real = real
+        @imaginary = imaginary
+    end
 
+    def to_s
+        "#{@real} + i*#{@imaginary}"
+    end
+
+    def real
+        @real 
+    end
+
+    def imaginary
+        @imaginary
+    end
+
+    def real=(real)
+        @real = real
+    end
+
+    def imaginary=(imaginary)
+        @imaginary = imaginary
+    end
+
+    def add(c)
+        MyComplex.new(real + c.real, imaginary + c.imaginary)
+    end
 end
